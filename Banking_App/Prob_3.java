eac910dd083297adb51d45b4f39ed954ca9d2138
NISA SOWBIKA <nisasowbika.ks2023cse@sece.ac.in>
2026-07-01 11:30:49 +0530

    Fix blue arrow folder and upload Java files

---
+++ Banking_App/Prob_3.java
@@ -0,0 +1,46 @@
+import java.util.*;
+class BankAccount
+{
+     private String accountNumber;
+     private String accountHolders;
+     private double balance;
+     public BankAccount(String accountNumber,String accountHolders,double balance)
+     {
+          this.accountNumber=accountNumber;
+          this.accountHolders=accountHolders;
+          this.balance=balance;
+     }
+     public void deposit(double amount)
+     {
+          if(amount<0||amount==0.0)
+          {
+               System.out.println("Invalid Deposit amount");
+          }
+          balance+=amount;
+     }
+     public void withdraw(double amount)
+     {
+          if(balance>=amount)
+          {
+               balance-=amount;
+          }
+          else
+          {
+               System.out.println("Insufficient Funds");
+          }
+     }
+     public double getBalance()
+     {
+          return balance;
+     }
+}
+public class Prob_3
+{
+     public static void main(String[] args)
+     {
+          BankAccount account = new BankAccount("ACC123", "John", 500.0);
+          account.deposit(200);
+          account.withdraw(800);
+          System.out.println(account.getBalance());
+     }
+}
